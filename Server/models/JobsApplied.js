bc2fed48097257c0643617ad81bccf154b7269a4
Deevyne99 <kaludivine545@gmail.com>
2024-03-06 17:29:53 +0100

    Model: Added user and AppliedJobs models

---
+++ Server/models/JobsApplied.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const JobApplied = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    require: [true, 'Please provide user'],
+  },
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+  },
+  company: String,
+  title: String,
+  location: String,
+  locationType: {
+    type: String,
+    enum: ['Remote', 'Onsite', 'Hybrid'],
+    default: 'Onsite',
+  },
+  dateApplied: {
+    type: Date,
+    default: Date.now,
+  },
+  platform: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Interview', 'Declined', 'Accepted'],
+    default: 'Pending',
+  },
+  minSalary: {
+    type: Number,
+    default: 0,
+  },
+  maxSalary: {
+    type: Number,
+    default: 0,
+  },
+})
+
+module.exports = mongoose.model('JobApplied', JobApplied)
