d8d70eff760d4a58b8585762467f1c9629ccf29a
Deevyne99 <kaludivine545@gmail.com>
2024-04-22 17:32:02 +0100

    Build: Setting up frontend

--- Jobhunt365/src/App.tsx
+++ Jobhunt365/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Navbar from './Components/Navbar'
+import Error from './pages/Error'
+
 function App() {
   return (
     <>
-      <div>hello world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path='*' element={<Error />} />
+          </Route>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ Jobhunt365/src/Components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from 'react-router-dom'
+
+// import React from 'react'
+Outlet
+
+const Navbar = () => {
+  return (
+    <header className='flex flex-col mx-4 md:mx-16'>
+      <nav className='flex justify-between '>
+        <h1>logo</h1>
+        <ul className='flex items-center gap-4'>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/login'}>login</Link>
+          <Link to={'/register'}>register</Link>
+        </ul>
+      </nav>
+      <Outlet />
+    </header>
+  )
+}
+
+export default Navbar
