cd563fd8d2db599279bde8d23335ff8512525312
Deevyne99 <kaludivine545@gmail.com>
2024-11-20 12:05:03 +0100

    Building home page

--- Jobhunt365/src/Components/Navbar.tsx
+++ Jobhunt365/src/Components/Navbar.tsx
@@ -5,10 +5,10 @@ import { Link, Outlet } from 'react-router-dom'
 const Navbar = () => {
   return (
     <main>
-      <header className='flex flex-col  bg-white py-2'>
-        <div className='flex flex-col mx-4 md:mx-16 '>
-          <nav className='flex justify-between items-center py-2'>
-            <h1>logo</h1>
+      <header className='flex flex-col bg-white   '>
+        <div className='flex flex-col mx-4 md:mx-16 py-2'>
+          <nav className='flex justify-between items-center py-4 px-8 fixed left-0 top-0 shadow-sm  w-full z-20 bg-white'>
+            <h1>JobHunt365</h1>
             <ul className='flex items-center gap-4'>
               <Link to={'/'}>Home</Link>
               <Link to={'/login'}>login</Link>
@@ -23,6 +23,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-
-
--- Jobhunt365/src/pages/Home.tsx
+++ Jobhunt365/src/pages/Home.tsx
@@ -1,7 +1,79 @@
 // import React from 'react'
 
+import image from '../assets/hero-section.jpg'
+
+import SuggestedJob from '../Components/ReuseableComponets/miniJobCard/SuggestedJob'
+const jobCategories = [
+  { id: 1, name: 'Software Development' },
+  { id: 2, name: 'Data Science' },
+  { id: 3, name: 'Marketing' },
+  { id: 4, name: 'Design' },
+  { id: 5, name: 'Healthcare' },
+  { id: 6, name: 'Education' },
+  { id: 7, name: 'Finance' },
+  { id: 8, name: 'Customer Service' },
+  { id: 9, name: 'Engineering' },
+  { id: 10, name: 'Human Resources' },
+  { id: 11, name: 'Sales' },
+  { id: 12, name: 'Logistics' },
+  { id: 13, name: 'Content Creation' },
+  { id: 14, name: 'Legal' },
+  { id: 15, name: 'Freelance & Remote Work' },
+  { id: 16, name: 'Freelance & Remote Work' },
+]
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='flex  flex-col '>
+      <div className='flex flex-col gap-4 md:gap-8 mt-24 mx-4 md:mx-12'>
+        <div className='flex justify-between'>
+          <article className='w-[50%] flex '>
+            <h2 className='text-2xl'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+              tenetur!
+            </h2>
+          </article>
+          <div className='w-[50%]'>
+            <img src={image} alt='' />
+          </div>
+        </div>
+        <div className='flex flex-col justify-center'>
+          <h2 className='text-2xl '>Recent Jobs</h2>
+          <div className='grid grid-cols-4  mt-6 justify-center gap-6 items-center '>
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+            <SuggestedJob />
+          </div>
+          <div className='flex  mt-12 flex-col justify-center'>
+            <h2 className='text-center text-2xl font-semibold'>
+              Trending Job Category
+            </h2>
+            <div className=' grid grid-cols-4 mt-6 gap-6 px-12'>
+              {jobCategories.map((category) => {
+                return (
+                  <div
+                    key={category.id}
+                    className='bg-white  text-[#94a3b8] font-bold mt p-2 flex rounded-md justify-center items-center hover:shadow-xl'
+                  >
+                    {' '}
+                    {category.name}
+                  </div>
+                )
+              })}
+            </div>
+            <button className='p-2 flex justify-center items-center mt-8 bg-[#60a5fa] text-white font-semibold rounded-md w-[200px] mx-auto'>
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
